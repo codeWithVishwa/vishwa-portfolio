0d59e5c2844638a17f663755d27de0c485d97128
codeWithVishwa <iamvishwasundar00@gmail.com>
2025-05-10 11:19:25 +0530

    Update app with new changes

--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -15,7 +15,7 @@ export const Home = () => {
         </p>
 
         <div className='flex justify-center space-x-4'>
-          <a href="#projects" className='bg-orange-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 165, 0,0.8)]'>View Projects</a>
+          <a href="#project" className='bg-orange-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 165, 0,0.8)]'>View Projects</a>
           <a href="#contact" className='border border-orange text-orange-500/70 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:bg-orange-500/10'> Contact Me</a>
 
         </div>
--- src/components/section/Project.jsx
+++ src/components/section/Project.jsx
@@ -4,7 +4,7 @@ import { RevealOnScroll } from "./RevealOnScroll";
 
 export const Project = () => {
   return (
-    <section id='projects' className='min-h-screen flex items-center justify-center py-20'>
+    <section id='project' className='min-h-screen flex items-center justify-center py-20'>
       <RevealOnScroll>
       <div className='max-w-5xl mx-auto px-4'>
         <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-orange-700 to-yellow-400 bg-clip-text text-transparent text-center'>Future Projects</h2>
